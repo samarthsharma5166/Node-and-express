bae90109aaf6e0f325903ee8cac4f5d86104bb40
Raman Kumar <rku.430@gmail.com>
2024-01-10 20:08:04 +0530

    Fixed typo and MVC

--- index.js
+++ index.js
@@ -9,23 +9,5 @@ connectToMongoDb("mongodb://localhost:27017/url")
 .catch((err)=>console.log({msg:err}));
 app.use(express.json());
 app.use("/url",urlRoute);
-app.get("/:shortId",async(req,res)=>{
-    const shortid = req.params.shortId;
-    console.log(shortid);
-    const entery = await URL.findOneAndUpdate(
-        {
-            ShortId: shortid
-        },
-        {
-            $push:{
-                viewHistory:{
-                    timestamp:Date.now()
-                },
-            },
-        }
-    );
-    console.log(entery);
-    res.redirect(entery.redirectUrl);
-})
 
 app.listen(port,()=>console.log("server is running at port 8000"))
--- routes/url.js
+++ routes/url.js
@@ -2,5 +2,5 @@ const express = require("express");
 const {handleGenrateNewShortUrl,getHandleUserUrl} = require("../controller/url");
 const router = express.Router();
 router.post("/",handleGenrateNewShortUrl);
-// router.get("/:shortId",getHandleUserUrl);
+router.get("/:shortId",getHandleUserUrl);
 module.exports=router;
